8b99f277408ada14f1249fa1fa7b6b5bb10fe65e
KreshiGoti <kreshigoti12@gmail.com>
2024-10-19 19:23:48 +0530

    Create Pra12.java

---
+++ PART-3/Pra12.java
@@ -0,0 +1,10 @@
+import java.util.*; 
+class Pra12 
+{ 
+public static void main(String args[]) 
+{ 
+int a= Integer.parseInt(args[0]); 
+int c = (a*100); 
+ System.out.println("your rupees is"+c);
+}
+};
