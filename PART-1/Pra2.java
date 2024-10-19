3a75e851e31887819182f6f4405ee0cc4c07adce
KreshiGoti <kreshigoti12@gmail.com>
2024-10-19 19:10:18 +0530

    Create Pra2.java

---
+++ PART-1/Pra2.java
@@ -0,0 +1,10 @@
+class Pra2 
+{ 
+public  static void main(String args[]) 
+{ 
+System.out.print("Your current balance is"); 
+int a = 20; 
+System.out.println(a); 
+System.out.println("23DCS036_kreshi_goti"); 
+} 
+};
