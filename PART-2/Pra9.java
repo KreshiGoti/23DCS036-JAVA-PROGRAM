b58964922f2900383eb8af42dc48086c69c8ddff
KreshiGoti <kreshigoti12@gmail.com>
2024-10-19 19:17:57 +0530

    Create Pra9.java

---
+++ PART-2/Pra9.java
@@ -0,0 +1,22 @@
+import java.util.*; 
+public class Pra9 { 
+   
+    public static void main(String[] args) { 
+ 
+       Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter a string: "); 
+        String str = sc.nextLine(); 
+        
+for(int i=0;i<str.length();i++)
+  { 
+char letter = str.charAt(i); 
+int count =0; 
+while(count != 2) 
+{ 
+System.out.print( letter); 
+count++; 
+} 
+} 
+System.out.println("23DCS036_kreshi_goti");      
+} 
+}; 
