ebef5a25d56cea703744ccf894d9c94dcf190962
KreshiGoti <kreshigoti12@gmail.com>
2024-10-19 19:20:07 +0530

    Create Pra11.java

---
+++ PART-2/Pra11.java
@@ -0,0 +1,14 @@
+import java.util.*; 
+ 
+class Pra11 { 
+    public static void main(String args[]) { 
+        
+        String str =new String("charusat university"); 
+ 
+        System.out.println("Your string is: " + str); 
+        System.out.println("Your lower case string is: " + str.toLowerCase()); 
+        System.out.println("replace h by first letter of my name string is: " +     
+str.replace('h','k')); 
+  System.out.println("23DCS036_kreshi_goti");   
+    }
+}
