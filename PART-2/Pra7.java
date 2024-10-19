c81dfc64610bd41e2ab16115cfa1d96f0b652e98
KreshiGoti <kreshigoti12@gmail.com>
2024-10-19 19:15:24 +0530

    Create Pra7.java

---
+++ PART-2/Pra7.java
@@ -0,0 +1,17 @@
+import java.util.*; 
+public class Pra7 { 
+public static void main(String[] args) { 
+for(int i=0;i<n;i++) 
+{ 
+Scanner sc = new Scanner(System.in); 
+System.out.print("Enter a string: "); 
+String str = sc.nextLine(); 
+System.out.print("Enter an integer how many times you want to repeat: "); 
+int n = sc.nextInt();
+for(int i=0;i<n;i++) 
+{ 
+System.out.print(str.substring(0,3)); 
+} 
+        System.out.println("23DCS036_kreshi_goti");      
+    } 
+}; 
